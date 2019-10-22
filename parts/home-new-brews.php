4f24695e6aac585a5c7b71a3f31994a546beb372
Ben Kaminski <ben@benkaminski.com>
2019-10-22 16:55:31 -0400

    did some tidy for development site

--- parts/home-new-brews.php
+++ parts/home-new-brews.php
@@ -4,7 +4,7 @@
 			<?php $loop = new WP_Query( array( 'post_type' => 'beers', 'posts_per_page' => 1 ));?>
 			<?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
 			<div class="col-md-12">
-				<h3 class="text-center pt-4 pb-4 text-uppercase">New at <?php echo get_bloginfo( 'name' ); ?></h3>
+				<h3 class="text-center pt-4 pb-4 text-uppercase">New at Wilmington Brew Works</h3>
 			</div>
 			<div class="card-deck pb-3">
 				<div class="col-lg-5 card rounded-0">
@@ -15,8 +15,11 @@
 					</div>
 				</div>
 				<div class="col-lg-7 card rounded-0">
-					<div class="card-body">
+					<div class="card-header">
 						<h4 class="card-title text-center">About <?php the_title(); ?></h4>
+					</div>
+					<div class="card-body">
+						
 						<?php the_excerpt(); ?>
 						
 					</div>
