20df0c0f493b98514d34dc18953892913aa19b94
Ben Kaminski <ben@benkaminski.com>
2019-12-08 19:02:06 -0500

    fixed hacks

--- parts/home-video.php
+++ parts/home-video.php
@@ -1,4 +1,4 @@
-	<h3 class="text-center pb-2">Welcome to Wilmington Brew Works</h3>
+	<h3 class="text-center pb-2">Welcome to <?php echo get_bloginfo(); ?></h3>
 	<div class="container-fluid kill-padding">
 		<div class="embed-responsive embed-responsive-16by9">
 			<iframe class="embed-responsive-item" width="560" height="315" src="https://www.youtube.com/embed/T5TW5XNeVYU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
--- parts/page-contact-us.php
+++ parts/page-contact-us.php
@@ -1,7 +1,7 @@
 <section class="contact pt-4 pb-4">
 	<div class="container text-center">
 		<h3>Questions?</h3>
-		<p>You have questions, we have answers. Contact us with any inquiries you may have.</p>
+		<p>You have questions, we have answers. Contact <?php echo get_bloginfo(); ?> with any inquiries you may have.</p>
 		<a class="btn btn-lg btn-blue rounded-0 pr-5 pl-5" href="contact">Contact Us</a>
 	</div>
 </section>
