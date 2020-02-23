7b53d775ef9d64d24faaea525092b599992d6173
Ben Kaminski <ben@benkaminski.com>
2020-02-23 16:20:21 -0500

    edited styles for to go page

--- page-togo.php
+++ page-togo.php
@@ -18,15 +18,13 @@ get_header(); ?>
 </div>
 <div class="container">
 	<main class="main-content-area pb-3 pt-3">
-		<div id="menu-container"></div>
 		
-	<script>
-
-		  !function(e,n){var t=document.createElement("script"),a=document.getElementsByTagName("script")[0];t.async=1,a.parentNode.insertBefore(t,a),t.onload=t.onreadystatechange=function(e,a){(a||!t.readyState||/loaded|complete/.test(t.readyState))&&(t.onload=t.onreadystatechange=null,t=void 0,a||n&&n())},t.src=e}("https://embed-menu-preloader.untappdapi.com/embed-menu-preloader.min.js",function(){PreloadEmbedMenu("menu-container",20943,79597)});
-		</script>
- 
 		<?php the_content(); ?>
-				
+
+		<div id="menu-container"></div>
+		<script>
+			!function(e,n){var t=document.createElement("script"),a=document.getElementsByTagName("script")[0];t.async=1,a.parentNode.insertBefore(t,a),t.onload=t.onreadystatechange=function(e,a){(a||!t.readyState||/loaded|complete/.test(t.readyState))&&(t.onload=t.onreadystatechange=null,t=void 0,a||n&&n())},t.src=e}("https://embed-menu-preloader.untappdapi.com/embed-menu-preloader.min.js",function(){PreloadEmbedMenu("menu-container",20943,79597)});
+		</script>
 	</main>
 	<div class="col-md-12 pt-3 pb-3 beer-tags shadow">
 		<?php the_tags('<span class="badge badge-secondary"><i class="fas fa-tags"></i> Related:</span>  ', ', '); ?>
